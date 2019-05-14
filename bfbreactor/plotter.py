531f2597edc01f0890017ce565a169b9e662b07a
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2019-05-14 14:27:42 -0400

    Move gas and mixture properties to gas-properties repo

--- bfbreactor/plotter.py
+++ bfbreactor/plotter.py
@@ -18,61 +18,6 @@ def _config(ax, xlabel, ylabel, title=None, loc=None):
         ax.legend(loc=loc)
 
 
-def plot_gas_properties(gases):
-    """
-    Plot molecular weight, viscosity, and density of several gases.
-    """
-    xticks_gas = range(len(gases))
-    xlabels_gas = [x.formula.translate(_sub) for x in gases]
-
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4), tight_layout=True)
-    for idx in range(len(gases)):
-        ax1.bar(xticks_gas[idx], gases[idx].mw, align='center', color='C0')
-        ax2.bar(xticks_gas[idx], gases[idx].mu, align='center', color='C4')
-        ax3.bar(xticks_gas[idx], gases[idx].rho, align='center', color='C9')
-    ax1.set_xticks(xticks_gas)
-    ax1.set_xticklabels(xlabels_gas)
-    ax1.set_ylabel('Molecular weight [g/mol]')
-    ax1.set_frame_on(False)
-    ax2.set_xticks(xticks_gas)
-    ax2.set_xticklabels(xlabels_gas)
-    ax2.set_ylabel('Viscosity [µP]')
-    ax2.set_frame_on(False)
-    ax3.set_xticks(xticks_gas)
-    ax3.set_xticklabels(xlabels_gas)
-    ax3.set_ylabel('Density [kg/m³]')
-    ax3.set_frame_on(False)
-
-
-def plot_mixture_properties(mixes, wts):
-    """
-    Plot molecular weight, viscosity, and density of several mixtures.
-    """
-    xticks_mix = range(len(mixes))
-    xlabels_mix = ['+'.join(x.mixture).translate(_sub) for x in mixes]
-
-    fig2, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4), tight_layout=True)
-    for idx in range(len(mixes)):
-        ax1.bar(xticks_mix[idx], mixes[idx].mw, align='center', color='C0')
-        ax1.text(xticks_mix[idx] - 0.3, mixes[idx].mw + 0.4, f'{wts[idx][0]}+{wts[idx][1]}', fontsize=9)
-        ax2.bar(xticks_mix[idx], mixes[idx].mu, align='center', color='C4')
-        ax2.text(xticks_mix[idx] - 0.3, mixes[idx].mu + 4, f'{wts[idx][0]}+{wts[idx][1]}', fontsize=9)
-        ax3.bar(xticks_mix[idx], mixes[idx].rho, align='center', color='C9')
-        ax3.text(xticks_mix[idx] - 0.3, mixes[idx].rho + 0.008, f'{wts[idx][0]}+{wts[idx][1]}', fontsize=9)
-    ax1.set_xticks(xticks_mix)
-    ax1.set_xticklabels(xlabels_mix)
-    ax1.set_ylabel('Molecular weight [g/mol]')
-    ax1.set_frame_on(False)
-    ax2.set_xticks(xticks_mix)
-    ax2.set_xticklabels(xlabels_mix)
-    ax2.set_ylabel('Viscosity [µP]')
-    ax2.set_frame_on(False)
-    ax3.set_xticks(xticks_mix)
-    ax3.set_xticklabels(xlabels_mix)
-    ax3.set_ylabel('Density [kg/m³]')
-    ax3.set_frame_on(False)
-
-
 def plot_umf_zexp(species, tc, umf, zexp):
     """
     Plot minimum fluidization velocity for a range of temperatures for different
