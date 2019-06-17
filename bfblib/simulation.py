c6a7ce0a944fd6f2b9cfea17d0b700f2b0bbefda
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2019-06-17 00:42:04 -0400

    Store Umf at temperature

--- bfblib/simulation.py
+++ bfblib/simulation.py
@@ -1,4 +1,5 @@
 import json
+import matplotlib.pyplot as plt
 import numpy as np
 import textwrap
 
@@ -56,6 +57,9 @@ class Simulation:
         tk_max = self.params.case['tk'][1]
         tks = np.arange(tk_min, tk_max + 10, 10)
 
+        umf_ergun = []
+        umf_wenyu = []
+
         for tk in tks:
             print(f'Running case at {tk} K ...')
 
@@ -67,22 +71,38 @@ class Simulation:
 
             # BFB model for fluidization
             bfb = BfbModel(gas, self.params)
-            bfb.solve()
 
-            # Particle model for biomass intra-particle heat conduction
-            prt = ParticleModel(gas, self.params)
-            prt.solve()
+            # Store Umf at temperature
+            umf_ergun.append(bfb.calc_umf_ergun(gas.mu))
+            umf_wenyu.append(bfb.calc_umf_wenyu(gas.mu))
 
-            # Pyrolysis model for biomass pyrolysis
-            pyro = PyrolysisModel(gas, self.params)
-            pyro.solve()
+            # # Particle model for biomass intra-particle heat conduction
+            # prt = ParticleModel(gas, self.params)
+            # prt.solve()
 
-            # Store results for temperature
-            results = {'tk': tk, **bfb.results, **prt.results, **pyro.results}
-            self.results_case.append(results)
+            # # Pyrolysis model for biomass pyrolysis
+            # pyro = PyrolysisModel(gas, self.params)
+            # pyro.solve()
 
-        print('Done.')
-        self.save_results()
+            # # Store results for temperature
+            # results = {'tk': tk, **bfb.results, **prt.results, **pyro.results}
+            # self.results_case.append(results)
+
+        fig, ax = plt.subplots(tight_layout=True)
+        ax.plot(tks, umf_ergun, '.-', label='Ergun')
+        ax.plot(tks, umf_wenyu, '.-', label="WenYu")
+        ax.fill_between(tks, umf_ergun, umf_wenyu, facecolor='0.9')
+        ax.grid(color='0.9')
+        ax.legend(loc='best')
+        ax.set_frame_on(False)
+        ax.set_xlabel('Temperature [K]')
+        ax.set_ylabel('Umf [m/s]')
+        ax.tick_params(color='0.9')
+
+        plt.show()
+
+        # print('Done.')
+        # self.save_results()
 
     def print_parameters(self):
         """
