ae096d05a77965cff0d0d901cded757440978a5f
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2019-07-06 16:57:01 -0400

    Rename simulation file to driver

---
+++ bfblib/driver.py
@@ -0,0 +1,134 @@
+import numpy as np
+
+from .gas import Gas
+from .particle import Particle
+from .bfb_model import BfbModel
+
+from .printer import print_parameters
+from .printer import print_gas_properties
+from .printer import print_particle_results
+from .printer import print_bfb_results
+
+from .plotter import plot_geldart
+from .plotter import plot_intra_particle_heat_cond
+from .plotter import plot_umf_temps
+from .plotter import plot_tdevol_temps
+from .plotter import plot_ut_temps
+
+
+def run_params(pm, path=None):
+
+    # Gas properties
+    # Note that gas mixture uses the Herning calculation for viscosity
+    gas = Gas(pm.gas['sp'], pm.gas['x'], pm.gas['p'], pm.gas['tk'])
+
+    # Bed particle
+    bed = Particle.from_params(pm.bed)
+    bed.calc_umf(pm.reactor['ep'], gas.mu, gas.rho)
+    bed.calc_ut(gas.mu, gas.rho)
+
+    # Biomass particle
+    bio = Particle.from_params(pm.biomass)
+    bio.calc_umf(pm.reactor['ep'], gas.mu, gas.rho)
+    bio.calc_ut(gas.mu, gas.rho)
+    bio.build_time_vector(pm.biomass['nt'], pm.biomass['t_max'])
+    bio.calc_trans_hc(pm.biomass['b'], pm.biomass['h'], pm.biomass['k'], pm.biomass['m'], pm.biomass['mc'], pm.biomass['tk_init'], gas.tk)
+    bio.calc_time_tkinf(gas.tk)
+    bio.calc_devol_time(gas.tk)
+
+    # Char particle
+    char = Particle.from_params(pm.char)
+    char.calc_umf(pm.reactor['ep'], gas.mu, gas.rho)
+    char.calc_ut(gas.mu, gas.rho)
+
+    # BFB reactor model
+    bfb = BfbModel(pm.reactor['di'], pm.reactor['q'], pm.reactor['zmf'])
+    bfb.calc_us(gas)
+    bfb.calc_us_umf(bed)
+    bfb.calc_tdh()
+    bfb.calc_zexp(bed, gas)
+
+    # Print parameters to screen
+    print(f"\n{' Parameters ':*^40}")
+    print_parameters(pm)
+
+    # Print results to screen
+    print(f"{' Results from Parameters ':*^40}")
+    print_gas_properties(gas)
+    print_particle_results(bed, bio, char)
+    print_bfb_results(bfb)
+
+    # Create and save plot figures if path is defined
+    if path is not None:
+        plot_geldart(gas, pm, path)
+        plot_intra_particle_heat_cond(bio, path)
+
+
+def run_temps(pm, path):
+    print(f"\n{' Simulate Temperatures ':*^40}\n")
+
+    tk_ref = pm.gas['tk']
+    tk_min = pm.case['tk'][0]
+    tk_max = pm.case['tk'][1]
+    tks = np.arange(tk_min, tk_max + 10, 10)
+
+    # Store Umf at each temperature from Ergun and WenYu equations
+    umf_ergun = []
+    umf_wenyu = []
+
+    # Store Ut at each temperature from Ganser and Haider equations
+    ut_bed_ganser = []
+    ut_bed_haider = []
+    ut_bio_ganser = []
+    ut_bio_haider = []
+    ut_char_ganser = []
+    ut_char_haider = []
+
+    # Store devolatilization time for each temperature
+    ts_devol = []
+
+    # Bed, biomass, and char particle
+    bed = Particle.from_params(pm.bed)
+    bio = Particle.from_params(pm.biomass)
+    char = Particle.from_params(pm.char)
+
+    # BFB reactor parameters
+    ep = pm.reactor['ep']
+
+    for tk in tks:
+        print(f'Run case at {tk} K ...')
+
+        # Gas properties at temperature
+        # Note that gas mixture uses the Herning calculation for viscosity
+        gas = Gas(pm.gas['sp'], pm.gas['x'], pm.gas['p'], tk)
+
+        # Bed particle calculations
+        bed.calc_umf(ep, gas.mu, gas.rho)
+        bed.calc_ut(gas.mu, gas.rho)
+
+        # Biomass particle calculations
+        bio.calc_ut(gas.mu, gas.rho)
+        bio.calc_devol_time(gas.tk)
+
+        # Char particle calculations
+        char.calc_ut(gas.mu, gas.rho)
+
+        # Append results for each temperature
+        umf_ergun.append(bed.umf_ergun)
+        umf_wenyu.append(bed.umf_wenyu)
+        ut_bed_ganser.append(bed.ut_ganser)
+        ut_bed_haider.append(bed.ut_haider)
+        ut_bio_ganser.append(bio.ut_ganser)
+        ut_bio_haider.append(bio.ut_haider)
+        ut_char_ganser.append(char.ut_ganser)
+        ut_char_haider.append(char.ut_haider)
+        ts_devol.append(bio.t_devol)
+
+    uts_ganser = {'bed': ut_bed_ganser, 'bio': ut_bio_ganser, 'char': ut_char_ganser}
+    uts_haider = {'bed': ut_bed_haider, 'bio': ut_bio_haider, 'char': ut_char_haider}
+
+    plot_umf_temps(tks, umf_ergun, umf_wenyu, tk_ref, path)
+    plot_ut_temps(tks, uts_ganser, uts_haider, path)
+    plot_tdevol_temps(tks, ts_devol, tk_ref, path)
+
+    print(f'Matplotlib figures saved to the `{path.name}` folder.\n')
